0e2bf0d64e8fc19d0ab266f1a7facd54cebd147c
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-04-21 07:12:01 +0100

    created

---
+++ backend/src/main/kotlin/com/adwi/buddy/backend/repository/cocktail/CocktailRepository.kt
@@ -0,0 +1,21 @@
+package com.adwi.buddy.backend.repository.cocktail
+
+import com.adwi.buddy.backend.models.Cocktail
+import com.adwi.buddy.backend.models.User
+import com.adwi.buddy.backend.repository.RepositoryInterface
+import com.adwi.buddy.backend.repository.userrepository.DATABASE_NAME
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoCollection
+import org.litote.kmongo.getCollection
+
+private const val COCKTAILS_COLLECTION = "cocktails"
+
+class CocktailRepository(client: MongoClient): RepositoryInterface<Cocktail> {
+
+    override lateinit var col: MongoCollection<Cocktail>
+
+    init {
+        val database = client.getDatabase(DATABASE_NAME)
+        col = database.getCollection()
+    }
+}
