3fc4da361c37246456a2edda46a3fd9c9b58f533
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-04-21 08:19:46 +0100

    removed interface UserRepository

--- backend/src/main/kotlin/com/adwi/buddy/backend/repository/cocktail/CocktailRepository.kt
+++ backend/src/main/kotlin/com/adwi/buddy/backend/repository/cocktail/CocktailRepository.kt
@@ -1,7 +1,6 @@
 package com.adwi.buddy.backend.repository.cocktail
 
 import com.adwi.buddy.backend.models.Cocktail
-import com.adwi.buddy.backend.models.User
 import com.adwi.buddy.backend.repository.RepositoryInterface
 import com.adwi.buddy.backend.repository.userrepository.DATABASE_NAME
 import com.mongodb.client.MongoClient
--- backend/src/main/kotlin/com/adwi/buddy/backend/repository/userrepository/UserRepository.kt
+++ backend/src/main/kotlin/com/adwi/buddy/backend/repository/userrepository/UserRepository.kt
@@ -2,7 +2,34 @@ package com.adwi.buddy.backend.repository.userrepository
 
 import com.adwi.buddy.backend.models.User
 import com.adwi.buddy.backend.repository.RepositoryInterface
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoCollection
+import org.litote.kmongo.eq
+import org.litote.kmongo.findOne
+import org.litote.kmongo.getCollection
 
-interface UserRepository : RepositoryInterface<User> {
-    fun getUserByEmail(email: String? = null): User?
+const val DATABASE_NAME = "cricket"
+private const val USER_COLLECTION = "users"
+
+class UserRepository(client: MongoClient) : RepositoryInterface<User> {
+
+    override lateinit var col: MongoCollection<User>
+
+    init {
+        val database = client.getDatabase(DATABASE_NAME)
+        col = database.getCollection()
+    }
+
+    fun getUserByEmail(email: String?): User? {
+        return try {
+            col.findOne(User::email eq email)
+        } catch (t: Throwable) {
+            throw Exception("Cannot get user with that email")
+        }
+    }
+
+    fun addCocktailToFavorites(cocktailId: String, userId: String): Boolean {
+        val user = getById(userId)
+
+    }
 }
